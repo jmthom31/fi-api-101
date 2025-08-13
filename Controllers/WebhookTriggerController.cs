3760a4f81a4fe80596ce6d48a8c3dd2e0c028e34
julia.thomas <julia.thomas@clear.bank>
2025-08-13 14:23:23 +0100

    Update endpoints

--- Controllers/WebhookTriggerController.cs
+++ Controllers/WebhookTriggerController.cs
@@ -1,56 +1,56 @@
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using WebhooksReceiver.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using System;
+//using System.Net.Http;
+//using System.Text;
+//using System.Text.Json;
+//using System.Threading.Tasks;
+//using WebhooksReceiver.Models;
 
-namespace WebhooksReceiver.Controllers;
+//namespace WebhooksReceiver.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-public class WebhookTriggerController : ControllerBase
-{
-    private readonly AuthProfile _authProfile;
+//[ApiController]
+//[Route("[controller]")]
+//public class WebhookTriggerController : ControllerBase
+//{
+//    private readonly AuthProfile _authProfile;
 
-    public WebhookTriggerController(AuthProfile authProfile)
-    {
-        _authProfile = authProfile;
-    }
+//    public WebhookTriggerController(AuthProfile authProfile)
+//    {
+//        _authProfile = authProfile;
+//    }
 
-    // This endpoint simulates sending a webhook to the listener. In production, this would be triggered by an event in your system.
-    [HttpPost]
-    [Route("trigger")]
-    public async Task<ActionResult<string>> Trigger([FromBody] ApiRequest request)
-    {
-        var webhookListenerUrl = "http://localhost:5000/sample/webhook";
+//    // This endpoint simulates sending a webhook to the listener. In production, this would be triggered by an event in your system.
+//    [HttpPost]
+//    [Route("trigger")]
+//    public async Task<ActionResult<string>> Trigger([FromBody] ApiRequest request)
+//    {
+//        var webhookListenerUrl = "http://localhost:5000/sample/webhook";
 
-        // Create simplified webhook request
-        var webhookRequest = new WebhookRequest
-        {
-            Type = "test.webhook",
-            Payload = new { FieldName = "test" },
-        };
+//        // Create simplified webhook request
+//        var webhookRequest = new WebhookRequest
+//        {
+//            Type = "test.webhook",
+//            Payload = new { FieldName = "test" },
+//        };
 
-        // Serialize the webhook payload to JSON
-        var webhookPayload = JsonSerializer.Serialize(webhookRequest);
+//        // Serialize the webhook payload to JSON
+//        var webhookPayload = JsonSerializer.Serialize(webhookRequest);
 
-        // Create HTTP request to send webhook
-        var requestMessage = new HttpRequestMessage(HttpMethod.Post, webhookListenerUrl);
-        requestMessage.Content = new StringContent(webhookPayload, Encoding.UTF8, "application/json");
-        requestMessage.Headers.Add("DigitalSignature", DigitalSignature.Generate(webhookPayload, _authProfile.ClientPrivateKey));
+//        // Create HTTP request to send webhook
+//        var requestMessage = new HttpRequestMessage(HttpMethod.Post, webhookListenerUrl);
+//        requestMessage.Content = new StringContent(webhookPayload, Encoding.UTF8, "application/json");
+//        requestMessage.Headers.Add("DigitalSignature", DigitalSignature.Generate(webhookPayload, _authProfile.ClientPrivateKey));
 
-        try
-        {
-            using var client = new HttpClient();
-            await client.SendAsync(requestMessage);
+//        try
+//        {
+//            using var client = new HttpClient();
+//            await client.SendAsync(requestMessage);
 
-            return Ok("Webhook sent successfully");
-        }
-        catch (Exception)
-        {
-            return BadRequest("Failed to send webhook");
-        }
-    }
-}
+//            return Ok("Webhook sent successfully");
+//        }
+//        catch (Exception)
+//        {
+//            return BadRequest("Failed to send webhook");
+//        }
+//    }
+//}
--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
-//using WebhooksReceiver.Endpoints;
+using WebhooksReceiver.Endpoints;
 
 namespace WebhooksReceiver;
 
@@ -25,19 +25,14 @@ public class Startup
 
         services.AddSingleton<AuthProfile>(provider => new AuthProfile
         {
-            ApiUrl = @"https://institution-api-sim.clearbank.co.uk",
+            //ApiUrl = @"https://institution-api-sim.clearbank.co.uk",
+            ApiUrl = @"https://testnarwhal-institution-api-uksouth.test-cbinnovation.uk",
             ApiToken = @"",
             ClientPrivateKey = @"-----BEGIN PRIVATE KEY-----
 
 -----END PRIVATE KEY-----",
             ClearBankPublicKey = @"-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhNgYwqIJYvV5tmouMTHS
-ThZI6lNSANvCs9Pr7n2SuSpEbqkkn1P8RXxHxdhEfiP95CC0WdO6HfDJEPr3wj1s
-dlVcpH9EC3/JJ/4s556jAmZpo2L5hQPDTOvu6figuvZMe0HaxbX5kOjrI7EX2rW4
-D7eLplcxzMoV6/O3GMHBTX4qgBBe+9Kxup9Ns191Xir21cnM6tb8/j6CuN0pEqru
-tyUOruCZCCWOxmMutN6g0C2fPuyii/N2bb2yNUp+A3/Tt26RMCTfYu73RESkxBci
-ORqIaLDZf5fwX7jhJojzSdlqSOBv+3OtmHEG77bHpnfti83LC+xQrEGe4MMZ85V5
-wwIDAQAB
+
 -----END PUBLIC KEY-----"
         });
     }
@@ -61,10 +56,10 @@ wwIDAQAB
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapControllers();
-            //endpoints.MapWebhookTriggerEndpoints();
-            //endpoints.MapApiTestEndpoints();
-            //endpoints.MapWebhooksReceiverEndpoints();
+            //endpoints.MapControllers();
+            endpoints.MapWebhookTriggerEndpoints();
+            endpoints.MapApiTestEndpoints();
+            endpoints.MapWebhooksReceiverEndpoints();
         });
     }
 }
